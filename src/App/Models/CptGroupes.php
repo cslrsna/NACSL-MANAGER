face662f1c3cfd49d71afc6040977b8a8179472f
LeCanardNoir <dev.lecanardnoir@gmail.com>
2019-07-30 14:21:13 -0400

    version 1.0.1

--- src/App/Models/CptGroupes.php
+++ src/App/Models/CptGroupes.php
@@ -40,7 +40,7 @@ abstract class CptGroupes implements CustomsDataInterface
         self::setName();
 
         $labels = array(
-            'name'                  => _x( 'Réunions de Narcotiques Anonymes', 'Post Type General Name', 'nacsl' ),
+            'name'                  => _x( 'Gestionnaires des réunions de Narcotiques Anonymes', 'Post Type General Name', 'nacsl' ),
             'singular_name'         => _x( 'Réunion', 'Post Type Singular Name', 'nacsl' ),
             'menu_name'             => __( 'Réunions', 'nacsl' ),
             'name_admin_bar'        => __( 'Réunions', 'nacsl' ),
