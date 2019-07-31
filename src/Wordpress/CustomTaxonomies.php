d6da9ee3ddce25ed21d5c23a429225224b9e7b6e
LeCanardNoir <dev.lecanardnoir@gmail.com>
2019-07-31 11:41:37 -0400

    version 1.0.1

--- src/Wordpress/CustomTaxonomies.php
+++ src/Wordpress/CustomTaxonomies.php
@@ -130,7 +130,9 @@ class CustomTaxonomies implements HooksInterfaces
 
     public function showTag($atts, $content)
     {
-        echo $content . get_the_term_list( get_the_ID(), $this->getName(), " ", " | ", " " );
+        if ( get_terms($this->getName()) ){
+            echo $content . get_the_term_list( get_the_ID(), $this->getName(), " ", " | ", " " );
+        }
     }
 
     public function register()
