929b45019c6c0defe7838af44e726e34840cb08b
LeCanardNoir <dev.lecanardnoir@gmail.com>
2019-08-01 20:32:47 -0400

    version 1.0.1

--- nacsl.php
+++ nacsl.php
@@ -41,7 +41,7 @@ $nacsl->execute(
 
         new CustomPostTypes( CptGroupes::data() ),
         new CustomPostTypes( CptSousComites::data() ),
-        new CustomPostTypes( CptServices::data() ),
+//        new CustomPostTypes( CptServices::data() ),
         new CustomPostTypes( CptActivites::data() ),
 
         new CustomTaxonomies( CptGroupes::getName(), TxFormats::data() ),
